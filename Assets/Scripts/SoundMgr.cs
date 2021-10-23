db25a56245c4ef0a9a41a6e152edd3602c43367f
Faris <cmsachanggeng@gmail.com>
2021-10-23 19:18:44 -0400

    AddSound and Delete Cooking Scene useless staff

---
+++ Assets/Scripts/SoundMgr.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundMgr : MonoBehaviour
+{
+    [Header("Audio List")]
+    [SerializeField] private List<AudioClip> bgmList;
+    [SerializeField] private List<AudioClip> dialoguesList;
+    [SerializeField] private static SoundMgr Instance = null;
+    [SerializeField] private int dialogueIndex = 0;
+
+    [Header("FadeIn Configs")]
+    [SerializeField] private float smoothness = 0.5f;
+    [SerializeField] private float increase = 0.05f;
+    [SerializeField] private float maxVolume = 1.0f;
+
+    private bool startBgm = true;
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.volume = 0;
+        PlayBGM(0);
+        VolumeFadeIn();
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
+    }
+
+    public void PlayDialogue()
+    {
+        if (dialogueIndex == dialoguesList.Count)
+        {
+            Debug.LogWarning("dialogue index exceeds range!");
+            return;
+        }
+        audioSource.PlayOneShot(dialoguesList[dialogueIndex]);
+        dialogueIndex++;
+    }
+
+    public void PlayBGM(int clipIndex)
+    {
+        audioSource.clip = bgmList[clipIndex];
+        audioSource.Play();
+    }
+
+    public void StopBGM()
+    {
+        audioSource.Stop();
+    }
+
+    private void VolumeFadeIn()
+    {
+        StartCoroutine(StartVolumeFadeIn(smoothness, increase, maxVolume));
+    }
+
+    IEnumerator StartVolumeFadeIn(float smoothness, float inc, float maxVol)
+    {
+        while (audioSource.volume < maxVol)
+        {
+            audioSource.volume += inc;
+            yield return new WaitForSeconds(smoothness);
+        }
+    }
+}
